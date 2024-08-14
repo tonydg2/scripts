1710524ce4ba344febabef678697d7b0d8610872
Tony <tonydgoodwin@gmail.com>
2024-08-14 12:01:59 -0400

    remove non u96 files

--- misc/beep.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DELAY=0.1;
-#echo -e "\a";sleep $DELAY;echo -e "\a";sleep $DELAY;echo -e "\a"
-echo -ne '\007';sleep $DELAY;echo -ne '\007';sleep $DELAY;echo -ne '\007'
--- misc/up.sh
+++
@@ -1,3 +0,0 @@
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
-echo DONE
-
